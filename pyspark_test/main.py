dd102ab9bed5c4372fa1a514ae7f931d6f2bad28
Sam Albertson <samalbertson3@gmail.com>
2024-11-09 09:10:11 -0800

    got pyspark + S3 upload working

--- pyspark_test/main.py
+++ pyspark_test/main.py
@@ -1,8 +1,15 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+import boto3
+import os
 
 # Initialize Spark session
-spark = SparkSession.builder.appName("SumColumn").getOrCreate()
+spark = (
+    SparkSession.builder.appName("SumColumn")
+    .config("spark.driver.extraJavaOptions", "-Djava.security.manager=allow")
+    .config("spark.executor.extraJavaOptions", "-Djava.security.manager=allow")
+    .getOrCreate()
+)
 
 # Read data from a CSV file
 df = spark.read.csv("pyspark_test/data.csv", header=True, inferSchema=True)
@@ -10,11 +17,17 @@ df = spark.read.csv("pyspark_test/data.csv", header=True, inferSchema=True)
 # Multiply the elements of columns 'x' and 'y' and create a new column 'z'
 df = df.withColumn("z", col("x") * col("y"))
 
-# Select only the 'z' column
-z_df = df.select("z")
-
 # Write the 'z' column to a new CSV file
-z_df.write.csv("s3://your-bucket-name/output/z_column", header=True, mode="overwrite")
+df.write.csv("pyspark_test/calc_data", header=True, mode="overwrite")
+
+# Upload to S3
+s3 = boto3.client("s3")
+for fn in os.listdir("pyspark_test/calc_data"):
+    s3.upload_file(
+        "pyspark_test/calc_data/" + fn,
+        "samalbertson3-test",
+        "pyspark/calc_data/" + fn,
+    )
 
 # Stop the Spark session
 spark.stop()
