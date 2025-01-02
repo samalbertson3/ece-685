aad886bfe02163d10684edc24a7e4802d0b7f6bd
Sam Albertson <samalbertson3@gmail.com>
2025-01-02 14:57:09 -0800

    confirmed runnable on EMR

--- pyspark_test/main.py
+++ pyspark_test/main.py
@@ -3,8 +3,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 import platform
+import pandas as pd
+from io import StringIO
+import boto3
 
-# import boto3
 # import os
 
 # Initialize Spark session
@@ -15,6 +17,14 @@ if platform.system() == "Windows":
         .config("spark.executor.extraJavaOptions", "-Djava.security.manager=allow")
         .getOrCreate()
     )
+
+    # Read data from a CSV file
+    # df = spark.read.csv("pyspark_test/data.csv", header=True, inferSchema=True)
+    s3 = boto3.client("s3")
+    obj = s3.get_object(Bucket="samalbertson3-test", Key="pyspark/data.csv")
+    csv = obj["Body"].read().decode("utf-8")
+    df = pd.read_csv(StringIO(csv))
+
 else:
     spark = (
         SparkSession.builder.appName("SumColumn").master("yarn")
@@ -23,12 +33,11 @@ else:
         .getOrCreate()
     )
 
+    df = spark.read.csv(
+        "s3://samalbertson3-test/pyspark_test/data.csv", header=True, inferSchema=True
+    )
 # spark.sparkContext.parallelize(range(1)).map(lambda x: "Hello World!").collect()
 
-
-## Read data from a CSV file
-# df = spark.read.csv("pyspark_test/data.csv", header=True, inferSchema=True)
-#
 ## Multiply the elements of columns 'x' and 'y' and create a new column 'z'
 # df = df.withColumn("z", col("x") * col("y"))
 #
@@ -36,7 +45,6 @@ else:
 # df.write.csv("pyspark_test/calc_data", header=True, mode="append")
 #
 ## Upload to S3
-# s3 = boto3.client("s3")
 # for fn in os.listdir("pyspark_test/calc_data"):
 #    s3.upload_file(
 #        "pyspark_test/calc_data/" + fn,
--- pyspark_test/setup.sh
+++ pyspark_test/setup.sh
@@ -4,6 +4,7 @@ sudo yum update -y
 sudo yum install python -y
 sudo yum install python3-pip -y
 
+pip install python-dateutil==2.8.2
 pip install boto3
 pip install pandas
 pip install pyspark
